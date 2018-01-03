910a56fa1ae2000a7f9c20ba416748c5df796a55
Danyal Imran <kool_danyal93@hotmail.com>
2018-01-04 00:28:48 +0500

    Added nav.js to assets/js directory

---
+++ assets/js/nav.js
@@ -0,0 +1,7 @@
+document.write('<ul class="links">\
+	<li><a href="index.html">Home</a></li>\
+	<li><a href="model.html">Models</a><li>\
+	<li><a href="lookup.html">Lookup</a></li>\
+	<li><a href="loopingDataSource.html">Looping DataSource</a></li>\
+	<li><a href="azure.html">Azure App Integration</a></li>\
+</ul>');
