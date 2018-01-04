34592e4c752cb799b90d68148ee411ce8ecdcb67
Danyal Imran <kool_danyal93@hotmail.com>
2018-01-05 01:09:22 +0500

    Added navigation submenu item names

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,7 +1,14 @@
 document.write('<ul class="links">\
 	<li><a href="index.html">Home</a></li>\
 	<li><a href="model.html">Models</a><li>\
+		<li><a href="model.html#createModel" class="subtopic">Create a model</a><li>\
+		<li><a href="model.html#updateModel" class="subtopic">Update a model</a><li>\
+		<li><a href="model.html#deleteModel" class="subtopic last">Delete a model</a><li>\
 	<li><a href="lookup.html">Lookup</a></li>\
+		<li><a href="lookup.html#staticLookup" class="subtopic">Static Lookup</a></li>\
+		<li><a href="lookup.html#dynamicLookup" class="subtopic last">Dynamic Lookup</a></li>\
 	<li><a href="loopingDataSource.html">Looping DataSource</a></li>\
+		<li><a href="loopingDataSource.html#loopingAll" class="subtopic">Looping all records</a></li>\
+		<li><a href="loopingDataSource.html#loopingMarked" class="subtopic last">Looping marked records</a></li>\
 	<li><a href="azure.html">Azure App Integration</a></li>\
 </ul>');
